f1a716fd9edaf44549fccb907bd9064606dd4901
rizkybaihaqy <rizkybaihaqy123@gmail.com>
2021-08-18 23:19:09 +0700

    add Button component to presentation page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Button Components</h1>
+      <div className="container">
+        <div className="card">
+          <span>&lt;Button /&gt;</span>
+          <Button>Default</Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button variant='outline' /&gt;</span>
+          <Button variant="outline" color="primary">
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button variant='text' /&gt;</span>
+          <Button variant="text" color="primary">
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button disableShadow /&gt;</span>
+          <Button disableShadow color="primary">
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button disabled /&gt;</span>
+          <Button disabled>Default</Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button variant="text" disabled /&gt;</span>
+          <Button variant="text" disabled>
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button startIcon="local_grocery_store" /&gt;</span>
+          <Button startIcon="local_grocery_store" color="primary">
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button endIcon="local_grocery_store" /&gt;</span>
+          <Button endIcon="local_grocery_store" color="primary">
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button size="sm" /&gt;</span>
+          <Button size="sm" color="primary">
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button size="md" /&gt;</span>
+          <Button size="md" color="primary">
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button size="lg" /&gt;</span>
+          <Button size="lg" color="primary">
+            Default
+          </Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button color="default" /&gt;</span>
+          <Button color="default">Default</Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button color="primary" /&gt;</span>
+          <Button color="primary">Default</Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button color="secondary" /&gt;</span>
+          <Button color="secondary">Default</Button>
+        </div>
+
+        <div className="card">
+          <span>&lt;Button color="danger" /&gt;</span>
+          <Button color="danger">Default</Button>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
